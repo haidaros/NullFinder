452aaa38459c58529ae540ef01f0130cb30730f1
Manuel Leuenberger <maenulabs@gmx.ch>
2015-02-20 14:23:41 +0100

    command line options, output to file instead of stdout

--- src/main/java/ch/unibe/scg/nullfinder/Main.java
+++ src/main/java/ch/unibe/scg/nullfinder/Main.java
@@ -1,20 +1,66 @@
 package ch.unibe.scg.nullfinder;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 
-	public static void main(String[] arguments) throws IOException,
-			NoSuchMethodException, SecurityException {
+	public static void main(String[] arguments) throws ParseException,
+			IOException {
+		Options options = new Options();
+		options.addOption(new Option("o", "output", true,
+				"CSV file to ouput to, defaults to stdout"));
+		CommandLineParser parser = new BasicParser();
+		CommandLine commandLine = parser.parse(options, arguments);
+		BufferedWriter output = new BufferedWriter(new OutputStreamWriter(
+				System.out));
+		boolean hasOutput = commandLine.hasOption("output");
+		if (hasOutput) {
+			Path outputPath = Paths.get(commandLine.getOptionValue("output"));
+			output = Files.newBufferedWriter(outputPath);
+		}
+		String[] inputs = commandLine.getArgs();
+		if (inputs.length == 0) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("NullFinder [options] input [input...]",
+					options);
+			System.exit(1);
+		}
+		BufferedWriter writer = output;
 		long before = System.currentTimeMillis();
-		Stream.of(arguments).map(Paths::get).flatMap(Main::extractAll)
-				.forEach(System.out::println);
+		System.out.println("PENDING processing...");
+		Stream.of(inputs).map(Paths::get).flatMap(Main::extractAll)
+				.forEach(line -> Main.write(writer, line));
+		writer.flush();
+		if (hasOutput) {
+			writer.close();
+		}
 		long after = System.currentTimeMillis();
 		System.out.println(String.format("DONE in %d seconds",
 				(after - before) / 1000));
+	}
+
+	private static void write(BufferedWriter writer, String line) {
+		try {
+			writer.write(line);
+			writer.newLine();
+		} catch (IOException exception) {
+			System.err.println(String.format("ERROR %s while writing %s",
+					exception.toString(), line));
+		}
 	}
 
 	private static Stream<String> extractAll(Path path) {
