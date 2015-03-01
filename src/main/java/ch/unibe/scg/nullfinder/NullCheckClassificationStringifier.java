8b25c696254a7c2e91ecef55111dd668cb2091f7
Manuel Leuenberger <maenulabs@gmx.ch>
2015-03-01 15:18:24 +0100

    use classifier class for stringification

--- src/main/java/ch/unibe/scg/nullfinder/NullCheckClassificationStringifier.java
+++ src/main/java/ch/unibe/scg/nullfinder/NullCheckClassificationStringifier.java
@@ -9,7 +9,6 @@ public class NullCheckClassificationStringifier {
 		return CSVFormat.DEFAULT.format(check.getPath().toString(), check
 				.getNode().getBeginLine(), check.getNode().getBeginColumn(),
 				check.getNode().getParentNode().toString(), classification
-						.getClass().getName());
+						.getClassifier().getClass().getName());
 	}
-
 }
